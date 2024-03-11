4e232af62df0cbdc5b76ce53eeb50ff80e8e59e3
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2024-03-11 14:21:12 +0700

    Update dependencies and fix types

--- src/utils/createAllowList.ts
+++ src/utils/createAllowList.ts
@@ -6,6 +6,26 @@ export interface AllowListCriteria {
   numberOfFollowersOnFarcaster?: number;
 }
 
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+  Address: { input: any; output: any; }
+  DateRange: { input: any; output: any; }
+  Identity: { input: any; output: any; }
+  IntString: { input: any; output: any; }
+  Map: { input: any; output: any; }
+  Range: { input: any; output: any; }
+  Time: { input: any; output: any; }
+  TimeRange: { input: any; output: any; }
+};
+
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+
 export type CreateAllowListQueryVariables = Exact<{
   identity: Scalars['Identity']['input'];
   eventIds: InputMaybe<Array<Scalars['String']['input']> | Scalars['String']['input']>;
